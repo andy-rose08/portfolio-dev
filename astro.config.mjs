10ce636cb416a8c2ff1d1bc1fef08949023a2fbc
andy-rose08 <leznedrios1208@gmail.com>
2024-03-17 19:50:32 -0600

    Improved looks of the portfolio and added modals to watch videos of the projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import reactRenderer from "@astrojs/renderer-react";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  renderers: [reactRenderer],
+  integrations: [tailwind(), react()]
 });
